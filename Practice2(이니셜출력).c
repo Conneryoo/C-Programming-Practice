4bc0b2a58568b719452712271fb20b6673a259c6
Conner Yoo <80835841+Conneryoo@users.noreply.github.com>
2024-06-04 14:00:44 +0900

    Update and rename Practice.c to Practice2(이니셜출력).c

---
+++ Practice2(이니셜출력).c
@@ -0,0 +1,13 @@
+    // 2. scanf를 써서 영문 이름 이니셜을 입력받고, 그걸 아래와 같이 출력하는 프로그램을 작성해보자.
+    //   >>LNY
+    //   >>Initials of your name are L, N and Y.
+    //   문자형 변수 사용
+    /*
+int main(void){
+     
+        char ini1, ini2, ini3;
+        printf("Enter initals of your name: ");
+        scanf("%c%c%c", &ini1, &ini2, &ini3);
+        printf("Initials of your name are %c, %c and %c.\n", ini1, ini2, ini3);
+        return 0;
+}
