800a6a88209bb40a50060c2645ae6755f80c5958
Conner Yoo <80835841+Conneryoo@users.noreply.github.com>
2024-06-04 14:03:42 +0900

    Create Practice7(홀짝판별).c

---
+++ Practice7(홀짝판별).c
@@ -0,0 +1,12 @@
+    // 7. 정수를 하나 입력받아, 짝수인지 홀수인지 판별하는 프로그램.
+    /*
+int main(void){
+    int num;
+    printf("정수를 입력하세요: ");
+    scanf("%d", &num);
+
+    if ((num % 2) == 0) {
+        printf("%d는 짝수 입니다.\n", num);
+    }else printf("%d는 홀수 입니다.\n", num);
+    return 0;
+}
