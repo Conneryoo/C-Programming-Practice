70c05dce84f1aafe63b738733d5bf51eecc7f5ec
Conner Yoo <80835841+Conneryoo@users.noreply.github.com>
2024-06-04 14:01:23 +0900

    Create Practice3(평균계산).c

---
+++ Practice3(평균계산).c
@@ -0,0 +1,12 @@
+    // 3. 네 정수를 입력하여 네 정수의 평균을 계산하여 출력하는 프로그램을 작성하시오.
+    //    평균은 실수가 되도록 계산하시오.
+    /*
+int main(void){
+        int a, b, c, d;
+        double average;
+        printf("Enter four integers.: ");
+        scanf("%d %d %d %d", &a, &b, &c, &d);
+        average = (a + b + c + d) / (double)4;
+        printf("The average of %d, %d, %d, %d is %.2lf", a, b, c, d, average);
+        return 0;
+}
